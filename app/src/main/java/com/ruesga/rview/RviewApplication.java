89498dc0670e92eccd58802e21089bf46d879b4c
Jorge Ruesga <jorge@ruesga.com>
2016-10-10 22:14:21 +0200

    Enable crashlytics and answers
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- app/src/main/java/com/ruesga/rview/RviewApplication.java
+++ app/src/main/java/com/ruesga/rview/RviewApplication.java
@@ -17,12 +17,29 @@ package com.ruesga.rview;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.answers.Answers;
+import com.crashlytics.android.core.CrashlyticsCore;
 import com.ruesga.rview.misc.Formatter;
+
+import io.fabric.sdk.android.Fabric;
+import io.fabric.sdk.android.Kit;
 
 public class RviewApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Install a hook to Crashlytics and Answers (only in production releases)
+        CrashlyticsCore core = new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build();
+        Crashlytics crashlytics = new Crashlytics.Builder().core(core).build();
+        Answers answers = new Answers();
+        Kit[] kits = BuildConfig.DEBUG ? new Kit[]{crashlytics} : new Kit[]{crashlytics, answers};
+        final Fabric fabric = new Fabric.Builder(this)
+                .kits(kits)
+                .debuggable(BuildConfig.DEBUG)
+                .build();
+        Fabric.with(fabric);
 
         // Initialize application resources
         Formatter.refreshCachedPreferences(getApplicationContext());
