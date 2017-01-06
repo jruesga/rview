633e03a5c3e516009dbaa3c8cdbb0307e4bc6814
Jorge Ruesga <jorge@ruesga.com>
2017-01-06 22:23:44 +0100

    Clean up
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- app/src/main/java/com/ruesga/rview/fragments/ChangeDetailsFragment.java
+++ app/src/main/java/com/ruesga/rview/fragments/ChangeDetailsFragment.java
@@ -42,7 +42,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.gson.reflect.TypeToken;
 import com.ruesga.rview.BaseActivity;
