8096384d15939cda61d4d8a95d9f5d3132b32064
Jorge Ruesga <jorge@ruesga.com>
2017-07-20 23:36:38 +0200

    Properly cast cherry-pick info
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- app/src/main/java/com/ruesga/rview/fragments/ChangeDetailsFragment.java
+++ app/src/main/java/com/ruesga/rview/fragments/ChangeDetailsFragment.java
@@ -2690,9 +2690,10 @@ public class ChangeDetailsFragment extends Fragment implements
                     mActionLoader.restart(ModelHelper.ACTION_REBASE, new String[]{(String) o[0]});
                     break;
                 case REQUEST_CODE_CHERRY_PICK:
+                    String[] result = (String[]) o[0];
                     mActionLoader.clear();
                     mActionLoader.restart(ModelHelper.ACTION_CHERRY_PICK,
-                            new String[]{(String) o[0], (String) o[1]});
+                            new String[]{result[0], result[1]});
                     break;
                 case REQUEST_CODE_MOVE_BRANCH:
                     mMoveBranchLoader.clear();
