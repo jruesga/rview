2e4df555ec9fee8583d8a6694ec584734adcfbc8
Jorge Ruesga <jorge@ruesga.com>
2017-10-14 13:19:37 +0200

    Made is_default compatible with older api version
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- gerrit/src/main/java/com/ruesga/rview/gerrit/model/DashboardInfo.java
+++ gerrit/src/main/java/com/ruesga/rview/gerrit/model/DashboardInfo.java
@@ -30,10 +30,14 @@ public class DashboardInfo {
     @SerializedName("foreach") public String foreach;
     @SerializedName("url") public String url;
     // Used by 2.14-
-    @SerializedName("default") public boolean defaultDashboard;
+    @SerializedName("default") private boolean defaultDashboard;
     // Used by 2.15+
-    @SerializedName("is_default") public boolean isDefaultDashboard;
+    @SerializedName("is_default") private boolean isDefaultDashboard;
     @SerializedName("title") public String title;
     @SerializedName("sections") public DashboardSectionInfo[] sections;
+
+    public boolean isDefaultDashboard() {
+        return defaultDashboard || isDefaultDashboard;
+    }
 }
 
