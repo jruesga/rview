2b19de5b79c160ff9df4ee4b7074240100313920
Jorge Ruesga <jorge@ruesga.com>
2017-10-28 00:20:34 +0200

    Adjust trending algorithm a bit
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- app/src/main/java/com/ruesga/rview/fragments/TrendingChangeListFragment.java
+++ app/src/main/java/com/ruesga/rview/fragments/TrendingChangeListFragment.java
@@ -45,6 +45,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TrendingChangeListFragment extends ChangeListByFilterFragment {
@@ -53,10 +54,13 @@ public class TrendingChangeListFragment extends ChangeListByFilterFragment {
 
     private static final int FETCH_COUNT = 75;
     private static final Pattern VOTE_PATTERN = Pattern.compile(".*[+-]\\d.*");
+    private static final Pattern INLINE_COMMENTS_PATTERN =
+            Pattern.compile(".*\\((\\d+) comment(s)?\\).*");
 
-    public static final int TRENDING_MAX_SCORE = 20;
+    public static final int TRENDING_MAX_SCORE = 25;
     private static final int TRENDING_MIN_SCORE = 8;
     private static final int TRENDING_MAX_HOURS = 36;
+    private static final int TRENDING_HOURS = 72;
 
     private boolean mForceRefresh;
 
@@ -149,15 +153,17 @@ public class TrendingChangeListFragment extends ChangeListByFilterFragment {
 
     private List<ChangeInfo> extractTrendingChanges(List<ChangeInfo> changes) {
         // TRENDING RULES
-        //  * Scoring over 20
+        //  * Scoring over 25
         //  * Max trending changes = account_fetched_items preference
         //  * Only changes with a score >= TRENDING_MIN_SCORE are tagged as trending changes
         //  * Score weight:
-        //      - Number of patchsets: 4 [3-20]
-        //      - Number of votes: 3 [1-10]
-        //      - Number of messages: 3 [1-20]
-        //      - Number of unique accounts commenting: 4 [3-7]
-        //      - Number of reviewers: 3 [5-15]
+        //      - Number of patchsets: 4 [5-10]
+        //      - Number of votes: 3 [1-4]
+        //      - Number of messages: 3 [7-20]
+        //      - Number of recent messages: 3 [1-3]
+        //      - Number of inline messages: 3 [1-10]
+        //      - Number of unique accounts commenting: 3 [3-7]
+        //      - Number of reviewers: 3 [1-15]
         //      - Updated: 3 [0 - TRENDING_MAX_HOURS]
 
         int maxItems = Preferences.getAccountFetchedItems(
@@ -167,12 +173,17 @@ public class TrendingChangeListFragment extends ChangeListByFilterFragment {
         for (ChangeInfo change : changes) {
             int maxAged = (int) (DateUtils.HOUR_IN_MILLIS * TRENDING_MAX_HOURS);
             int aged = maxAged - (int)(System.currentTimeMillis() - change.updated.getTime());
+            long trendingAged = maxAged - (int)(System.currentTimeMillis() -
+                    (DateUtils.HOUR_IN_MILLIS * TRENDING_HOURS));
+
             change.trendingScore =
-                    applyWeight(countNumberOfPatchsets(change), 4f, 3, 20) +
-                    applyWeight(countNumberOfVotesOfNotRobotAccounts(change), 3f, 1, 10) +
-                    applyWeight(countNumberOfMessagesFromNotRobotAccounts(change), 3f, 1, 20) +
-                    applyWeight(countNumberOfNotRobotAccountsWithAComment(change), 4f, 3, 7) +
-                    applyWeight(countNumberOfReviewers(change), 3f, 5, 15) +
+                    applyWeight(countNumberOfPatchsets(change), 4f, 5, 10) +
+                    applyWeight(countNumberOfVotesOfNotRobotAccounts(change, -1L), 3f, 1, 4) +
+                    applyWeight(countNumberOfMessagesFromNotRobotAccounts(change, -1L), 3f, 7, 20) +
+                    applyWeight(countNumberOfMessagesFromNotRobotAccounts(change, trendingAged), 3f, 1, 3) +
+                    applyWeight(countNumberOfInlineMessagesFromNotRobotAccounts(change, -1L), 3f, 1, 10) +
+                    applyWeight(countNumberOfNotRobotAccountsWithAComment(change,-1L), 3f, 3, 7) +
+                    applyWeight(countNumberOfReviewers(change), 3f, 1, 15) +
                     applyWeight(aged, 3f, 0, maxAged);
             if (change.trendingScore >= TRENDING_MIN_SCORE) {
                 trending.add(change);
@@ -207,12 +218,30 @@ public class TrendingChangeListFragment extends ChangeListByFilterFragment {
     }
 
     // Counts the number of comments from user accounts (excluding robot accounts)
-    private int countNumberOfMessagesFromNotRobotAccounts(ChangeInfo change) {
+    private int countNumberOfMessagesFromNotRobotAccounts(ChangeInfo change, long maxAged) {
         int count = 0;
         if (change.messages != null) {
             for (ChangeMessageInfo message : change.messages) {
-                if (TextUtils.isEmpty(message.tag)) {
+                if (TextUtils.isEmpty(message.tag) &&
+                        (maxAged == -1 || message.date.getTime() < maxAged)) {
                     count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    // Counts the number of inline comments from user accounts (excluding robot accounts)
+    private int countNumberOfInlineMessagesFromNotRobotAccounts(ChangeInfo change, long maxAged) {
+        int count = 0;
+        if (change.messages != null) {
+            for (ChangeMessageInfo message : change.messages) {
+                if (TextUtils.isEmpty(message.tag) &&
+                        (maxAged == -1 || message.date.getTime() < maxAged)) {
+                    Matcher m = INLINE_COMMENTS_PATTERN.matcher(message.message);
+                    while (m.find()) {
+                        count += Integer.parseInt(m.group(1));
+                    }
                 }
             }
         }
@@ -221,11 +250,12 @@ public class TrendingChangeListFragment extends ChangeListByFilterFragment {
 
     // Counts the number of user accounts with a comment (excluding robot accounts
     // and voting only messages)
-    private int countNumberOfNotRobotAccountsWithAComment(ChangeInfo change) {
+    private int countNumberOfNotRobotAccountsWithAComment(ChangeInfo change, long maxAged) {
         Set<Integer> accounts = new HashSet<>();
         if (change.messages != null) {
             for (ChangeMessageInfo message : change.messages) {
-                if (TextUtils.isEmpty(message.tag) && message.message.indexOf("\n\n") > 0) {
+                if (TextUtils.isEmpty(message.tag) && message.message.indexOf("\n\n") > 0 &&
+                        (maxAged == -1 || message.date.getTime() < maxAged)) {
                     accounts.add(message.author.accountId);
                 }
             }
@@ -235,11 +265,12 @@ public class TrendingChangeListFragment extends ChangeListByFilterFragment {
 
     // Count the number of votes (activity registered by voting/remove voting) of user accounts
     // (excluding robot accounts)
-    private int countNumberOfVotesOfNotRobotAccounts(ChangeInfo change) {
+    private int countNumberOfVotesOfNotRobotAccounts(ChangeInfo change, long maxAged) {
         int count = 0;
         if (change.labels != null) {
             for (ChangeMessageInfo message : change.messages) {
-                if (TextUtils.isEmpty(message.tag)) {
+                if (TextUtils.isEmpty(message.tag) &&
+                        (maxAged == -1 || message.date.getTime() < maxAged)) {
                     String firstLine = message.message;
                     int pos = message.message.indexOf("\n");
                     if (pos > 0) {
