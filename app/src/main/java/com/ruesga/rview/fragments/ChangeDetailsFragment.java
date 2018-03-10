jruesga/rview

commit 2b07acb0ca9a8e37b3aa219cbe1f818815dd3844

Support reviewer updates in 2.15
Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

app/src/main/java/com/ruesga/rview/fragments/ChangeDetailsFragment.java
@@ -107,6 +107,7 @@ import com.ruesga.rview.gerrit.model.ReviewInfo;
 import com.ruesga.rview.gerrit.model.ReviewInput;
 import com.ruesga.rview.gerrit.model.ReviewerInput;
 import com.ruesga.rview.gerrit.model.ReviewerStatus;
+import com.ruesga.rview.gerrit.model.ReviewerUpdateInfo;
 import com.ruesga.rview.gerrit.model.RevisionInfo;
 import com.ruesga.rview.gerrit.model.SideType;
 import com.ruesga.rview.gerrit.model.SubmitInput;
@@ -194,6 +195,7 @@ public class ChangeDetailsFragment extends Fragment implements
         add(ChangeOptions.CHECK);
         add(ChangeOptions.WEB_LINKS);
         add(ChangeOptions.DOWNLOAD_COMMANDS);
+        add(ChangeOptions.REVIEWER_UPDATES);
     }};
     private static final List<ChangeOptions> MESSAGES_OPTIONS = new ArrayList<ChangeOptions>() {{
         add(ChangeOptions.DETAILED_ACCOUNTS);
@@ -675,8 +677,10 @@ public class ChangeDetailsFragment extends Fragment implements
         }
 
         void update(ListModel listModel, ChangeMessageInfo[] messages,
-                Map<String, LinkedHashMap<String, List<CommentInfo>>> messagesWithComments) {
-            mMessages = filterTaggedMessages(filterCiAccountsMessages(messages));
+                Map<String, LinkedHashMap<String, List<CommentInfo>>> messagesWithComments,
+                ReviewerUpdateInfo[] reviewerUpdate) {
+            mMessages = filterTaggedMessages(filterCiAccountsMessages(
+                    joinWithReviewerUpdates(messages, reviewerUpdate)));
             mMessagesWithComments = messagesWithComments;
 
             int count = mMessages.length;
@@ -768,6 +772,48 @@ public class ChangeDetailsFragment extends Fragment implements
             }
             return msgs.toArray(new ChangeMessageInfo[msgs.size()]);
         }
+
+        private ChangeMessageInfo[] joinWithReviewerUpdates(ChangeMessageInfo[] messages,
+                ReviewerUpdateInfo[] reviewerUpdate) {
+            if (mIsHideTaggedMessages) {
+                return messages;
+            }
+            if (reviewerUpdate == null || reviewerUpdate.length == 0) {
+                return messages;
+            }
+
+            List<ChangeMessageInfo> msgs = new ArrayList<>(Arrays.asList(messages));
+            for (ReviewerUpdateInfo ru : reviewerUpdate) {
+                int i = 0;
+                boolean found = false;
+                for (ChangeMessageInfo msg : msgs) {
+                    if (msg.date.compareTo(ru.updated) > 0) {
+                        ChangeMessageInfo newMessage = new ChangeMessageInfo();
+                        newMessage.id = String.valueOf(ru.updated.getTime());
+                        newMessage.date = ru.updated;
+                        newMessage.author = ru.updatedBy;
+                        newMessage.message = ru.state.name() + ": " + ModelHelper.getAccountDisplayName(ru.reviewer);
+                        newMessage.tag = "autogenerated:gerrit:reviewer_update";
+                        msgs.add(i, newMessage);
+                        found = true;
+                        break;
+                    }
+                    i++;
+                }
+
+                if (!found) {
+                    ChangeMessageInfo newMessage = new ChangeMessageInfo();
+                    newMessage.id = String.valueOf(ru.updated.getTime());
+                    newMessage.date = ru.updated;
+                    newMessage.author = ru.updatedBy;
+                    newMessage.message = ru.state.name() + ": " + ModelHelper.getAccountDisplayName(ru.reviewer);
+                    newMessage.tag = "autogenerated:gerrit:reviewer_update";
+                    msgs.add(newMessage);
+                }
+            }
+
+            return msgs.toArray(new ChangeMessageInfo[msgs.size()]);
+        }
     }
 
     private static class DataResponse {
@@ -843,7 +889,7 @@ public class ChangeDetailsFragment extends Fragment implements
                             : R.string.change_details_action_hide_ci_messages));
                 }
                 mMessageAdapter.update(mModel.msgListModel, change.messages,
-                        result.mMessagesWithComments);
+                        result.mMessagesWithComments, change.reviewerUpdates);
             }
 
             // Invalidate the diff cache. we have new data
@@ -963,7 +1009,7 @@ public class ChangeDetailsFragment extends Fragment implements
             ModelHelper.updateChangeMessageInfo(
                     getActivity(), mAccount, mResponse.mChange, review.first);
             mMessageAdapter.update(mModel.msgListModel, mResponse.mChange.messages,
-                    mResponse.mMessagesWithComments);
+                    mResponse.mMessagesWithComments, mResponse.mChange.reviewerUpdates);
 
             // Fetch the whole change
             forceRefresh();
@@ -1071,7 +1117,8 @@ public class ChangeDetailsFragment extends Fragment implements
             // will fix the out-of-sync problem).
             mResponse.mChange.messages = messages;
             mModel.msgListModel.visible = messages != null && messages.length > 0;
-            mMessageAdapter.update(mModel.msgListModel, messages, mResponse.mMessagesWithComments);
+            mMessageAdapter.update(mModel.msgListModel, messages, mResponse.mMessagesWithComments,
+                    mResponse.mChange.reviewerUpdates);
             mBinding.setModel(mModel);
         }
 
@@ -2536,7 +2583,7 @@ public class ChangeDetailsFragment extends Fragment implements
 
                 mMessageAdapter.updateHideTaggedMessages(mHideTaggedMessages);
                 mMessageAdapter.update(mModel.msgListModel, mResponse.mChange.messages,
-                        mResponse.mMessagesWithComments);
+                        mResponse.mMessagesWithComments, mResponse.mChange.reviewerUpdates);
                 mBinding.setModel(mModel);
             }
             mModel.isLocked = false;
@@ -2558,7 +2605,7 @@ public class ChangeDetailsFragment extends Fragment implements
                 }
                 mMessageAdapter.updateHideCIMessages(repo);
                 mMessageAdapter.update(mModel.msgListModel, mResponse.mChange.messages,
-                        mResponse.mMessagesWithComments);
+                        mResponse.mMessagesWithComments, mResponse.mChange.reviewerUpdates);
                 updateChangeInfo(mResponse);
                 mBinding.setModel(mModel);
             }

commit ce0141d07d8cded1d4f1fc41b12aed441eda6b53

Fix regression introduced in 2b07acb0ca9a8e37b3aa219cbe1f818815dd3844 (messages reference is locally changed but not globally updated)
Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

app/src/main/java/com/ruesga/rview/fragments/ChangeDetailsFragment.java
@@ -677,12 +677,13 @@ public class ChangeDetailsFragment extends Fragment implements
             mRepository = repo;
         }
 
-        void update(ListModel listModel, ChangeMessageInfo[] messages,
+        void update(ListModel listModel, ChangeInfo change,
                 Map<String, LinkedHashMap<String, List<CommentInfo>>> messagesWithComments,
                 ReviewerUpdateInfo[] reviewerUpdates) {
             mMessages = filterTaggedMessages(filterCiAccountsMessages(
-                    joinWithReviewerUpdates(messages, reviewerUpdates)));
+                    joinWithReviewerUpdates(change.messages, reviewerUpdates)));
             mMessagesWithComments = messagesWithComments;
+            change.messages = mMessages;
 
             int count = mMessages.length;
             boolean[] old = mFolded;
@@ -903,7 +904,7 @@ public class ChangeDetailsFragment extends Fragment implements
                             ? R.string.change_details_action_show_ci_messages
                             : R.string.change_details_action_hide_ci_messages));
                 }
-                mMessageAdapter.update(mModel.msgListModel, change.messages,
+                mMessageAdapter.update(mModel.msgListModel, change,
                         result.mMessagesWithComments, change.reviewerUpdates);
             }
 
@@ -1023,7 +1024,7 @@ public class ChangeDetailsFragment extends Fragment implements
             // update the message list until a full refresh happens)
             ModelHelper.updateChangeMessageInfo(
                     getActivity(), mAccount, mResponse.mChange, review.first);
-            mMessageAdapter.update(mModel.msgListModel, mResponse.mChange.messages,
+            mMessageAdapter.update(mModel.msgListModel, mResponse.mChange,
                     mResponse.mMessagesWithComments, mResponse.mChange.reviewerUpdates);
 
             // Fetch the whole change
@@ -1133,7 +1134,7 @@ public class ChangeDetailsFragment extends Fragment implements
             mResponse.mChange.messages = change.messages;
             mResponse.mChange.reviewerUpdates = change.reviewerUpdates;
             mModel.msgListModel.visible = change.messages != null && change.messages.length > 0;
-            mMessageAdapter.update(mModel.msgListModel, change.messages,
+            mMessageAdapter.update(mModel.msgListModel, change,
                     mResponse.mMessagesWithComments, mResponse.mChange.reviewerUpdates);
             mBinding.setModel(mModel);
         }
@@ -2596,7 +2597,7 @@ public class ChangeDetailsFragment extends Fragment implements
                         : R.string.change_details_action_hide_tagged_messages));
 
                 mMessageAdapter.updateHideTaggedMessages(mHideTaggedMessages);
-                mMessageAdapter.update(mModel.msgListModel, mResponse.mChange.messages,
+                mMessageAdapter.update(mModel.msgListModel, mResponse.mChange,
                         mResponse.mMessagesWithComments, mResponse.mChange.reviewerUpdates);
                 mBinding.setModel(mModel);
             }
@@ -2618,7 +2619,7 @@ public class ChangeDetailsFragment extends Fragment implements
                     repo = ModelHelper.findRepositoryForAccount(getContext(), mAccount);
                 }
                 mMessageAdapter.updateHideCIMessages(repo);
-                mMessageAdapter.update(mModel.msgListModel, mResponse.mChange.messages,
+                mMessageAdapter.update(mModel.msgListModel, mResponse.mChange,
                         mResponse.mMessagesWithComments, mResponse.mChange.reviewerUpdates);
                 updateChangeInfo(mResponse);
                 mBinding.setModel(mModel);