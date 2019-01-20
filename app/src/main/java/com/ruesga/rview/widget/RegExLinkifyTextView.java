9abfdcbd18cd8e9d11632ffd2f3546aa3552a535
Jorge Ruesga <jorge@ruesga.com>
2019-01-20 14:28:30 +0100

    Remove unused import
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- app/src/main/java/com/ruesga/rview/widget/RegExLinkifyTextView.java
+++ app/src/main/java/com/ruesga/rview/widget/RegExLinkifyTextView.java
@@ -23,7 +23,6 @@ import android.text.Spanned;
 import android.text.style.ClickableSpan;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.util.SparseIntArray;
 import android.view.SoundEffectConstants;
 import android.view.View;
 
